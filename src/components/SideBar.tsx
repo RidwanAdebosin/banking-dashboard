f892f24e1b8e438e284e677ad400968ebf840652
RidwanAdebosin <adebosinadewale@gmail.com>
2024-11-14 23:45:11 +0100

    feat: Toggle button for light abd dark mode built

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -18,7 +18,7 @@ const SideBar = () => {
   return (
     <>
       <div>
-        {expandNav && (
+        {expandNav ? (
           <div className=" h-[50%] flex flex-col gap-4 justify-center items-start text-white pt-[100px] px-4">
             <Link to="/" className="hover:rounded-full hover:bg-slate-400">
               <li className="list-type-none flex items-center gap-1 p-2">
@@ -49,47 +49,61 @@ const SideBar = () => {
               </li>
             </Link>
           </div>
+        ) : (
+          <div className=" h-[50%]  flex-col gap-4 justify-center items-start text-white pt-[100px] px-4">
+            <Link to="/">
+              <li className="list-type-none flex items-center gap-1 p-2 hover:rounded-full hover:bg-slate-400">
+                <span>
+                  <FaHome />
+                </span>
+                Home
+              </li>
+            </Link>
+
+            <Link to="transactions-page">
+              <li className="list-type-none flex items-center gap-1 p-2 hover:rounded-full hover:bg-slate-400">
+                <span>
+                  <FaDatabase />
+                </span>
+                Transactions
+              </li>
+            </Link>
+            <Link to="customers-page">
+              <li className="list-type-none flex items-center gap-1 p-2 hover:rounded-full hover:bg-slate-400">
+                <span>
+                  <FaBook />
+                </span>
+                Customers
+              </li>
+            </Link>
+          </div>
         )}
 
-        <div className=" h-[50%]  flex-col gap-4 justify-center items-start text-white pt-[100px] px-4">
-          <Link to="/" className="hover:rounded-full hover:bg-slate-400 ">
-            <li className="list-type-none flex items-center gap-1 p-2">
-              <span>
-                <FaHome />
-              </span>
-              Home
-            </li>
-          </Link>
-
-          <Link
-            to="transactions-page"
-            className="hover:rounded-full hover:bg-slate-400"
+        <div className="px-4">
+          <button
+            onClick={() => handleExpandNav()}
+            className="px-2  py-2 mt-8 cursor-pointer hover:rounded-full hover:bg-slate-400 block text-white"
+            role="button"
           >
-            <li className="list-type-none flex items-center gap-1 p-2">
-              <span>
-                <FaDatabase />
-              </span>
-              Transactions
-            </li>
-          </Link>
-          <Link
-            to="customers-page"
-            className="hover:rounded-full hover:bg-slate-400"
-          >
-            <li className="list-type-none flex items-center gap-1 p-2">
-              <span>
-                <FaBook />
-              </span>
-              Customers
-            </li>
-          </Link>
+            {expandNav ? <FaAngleDoubleRight /> : <FaAngleDoubleLeft />}
+          </button>
         </div>
 
-        <div
-          onClick={() => handleExpandNav()}
-          className=" px-6 py-2 mt-8 cursor-pointer hover:rounded-full hover:bg-slate-400 block text-white"
-        >
-          {expandNav ? <FaAngleDoubleRight /> : <FaAngleDoubleLeft />}
+        <div className="pl-6 py-2 mt-8 cursor-pointer" aria-role="switch">
+          <label
+            htmlFor="switch"
+            className=" cursor-pointer text-white text-sm pb-2"
+          >
+            Change theme
+            <input
+              id="switch"
+              type="checkbox"
+              value=""
+              role="switch"
+              className="sr-only peer"
+            />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-black"></div>
+          </label>
         </div>
       </div>
     </>
